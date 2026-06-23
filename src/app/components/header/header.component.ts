b327aff429123bf8d60a5b67a25f4db7b989309b
Leo7479 <shubhrajitsarkar123ss@gmail.com>
2026-06-23 11:55:31 +0530

    Added Mobile device compatibility

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,12 +2,27 @@ import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
 
 @Component({
-    selector: 'app-header', 
-    standalone: true, 
-    imports: [RouterLink], 
+    selector: 'app-header',
+    standalone: true,
+    imports: [RouterLink],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
-export class Header{
+export class Header {
+    onHamburgerMenuClick(e: Event, navRef: HTMLDivElement) {
+        if (navRef.style.left === '' || navRef.style.left === '200%') {
+            console.log("opening");
+            navRef.style.left = '0px';
+        }
+        else {
+            console.log("closing");
+            navRef.style.left = '200%';
+        }
+    }
 
+    onNavSectionClick(e: Event) {
+        if (e.target instanceof HTMLLIElement || e.target instanceof HTMLAnchorElement) {
+            this.onHamburgerMenuClick(e, e.currentTarget as HTMLDivElement);
+        }
+    }
 }
