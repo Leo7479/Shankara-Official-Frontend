f1152e2c188280775402096ffe509a86c57f5aed
Leo7479 <shubhrajitsarkar123ss@gmail.com>
2026-06-23 12:49:55 +0530

    Added spinner with minimum duration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { forkJoin, fromEvent, mapTo, merge, of, take, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('shankara-official-frontend');
+  loading = signal(true);
+
+  ngAfterViewInit(): void {
+    const minDuration$ = timer(1000);
+    const load$ = (document.readyState==='complete')?of(true):fromEvent(window, 'load').pipe(take(1));
+
+    forkJoin([
+      load$, 
+      minDuration$
+    ]).subscribe(()=>{
+      this.loading.set(false);
+    })
+  }
 }
